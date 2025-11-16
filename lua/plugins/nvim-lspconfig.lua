65f30e7c079c563255afbe5871e485ed70909c24
William Strong <williamstrong01@gmail.com>
2025-11-16 10:58:18 -0500

    Clangd CUDA setup

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -108,28 +108,32 @@ return {
           },
         },
       },
+      clangd = {
+        cmd = { 'clangd', '--log=verbose' },
+        init_options = {
+          compilationDatabasePath = './build/',
+        },
+      },
     }
 
     -- Mason setup handled here (:Mason + g? for help)
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
       'stylua',
+      -- clangd snapshot build to fix CUDA issue
+      { 'clangd', version = 'snapshot_20251109' },
     })
+
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+    -- Setup config for each server per 2.0.0 release
+    for server, config in pairs(servers) do
+      vim.lsp.config(server, config)
+    end
 
     require('mason-lspconfig').setup {
       ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
       automatic_installation = false,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for ts_ls)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
     }
   end,
 }
